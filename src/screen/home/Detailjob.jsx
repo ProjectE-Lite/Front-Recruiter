afb9297e654a0e74d1bfe04e26580439abd533c4
Wittawat <wittawat.pho@ku.th>
2023-10-10 16:07:36 +0700

    POST method

--- src/screen/home/Detailjob.jsx
+++ src/screen/home/Detailjob.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import { View, Text, Image, Button, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { YOURAPI } from '../../constants/editendpoint';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Detailjob({route}) {
+  const navigation = useNavigation()
   const initialFormData= route.params
   const [Data, setData] = useState(initialFormData)
-
+  const recruiter_id = "6517fa561434530638bc81de"
   const updateDescription = (field, value) => {
     setData(prevState => ({
       ...prevState,
       work_description: {
         ...prevState.work_description,
-        [field]: value
+        [field]: value.replace(/\n/g, '\\n')
       }
     }));
   };
   console.log(Data)
 
   const handlePostData = () => {
-    axios.post(``, jobData)
+    axios.post(`http://${YOURAPI}/recruiters/${recruiter_id}/works`, Data)
       .then(response => {
         console.log('Success:', response.data);
+        navigation.navigate('pageHome')
       })
       .catch(error => {
         console.error('Error:', error);
@@ -46,7 +51,7 @@ export default function Detailjob({route}) {
           style={{fontSize: 17, color: 'white'}} 
           placeholder='กรอกเป็นข้อ1. 2. 3.' 
           placeholderTextColor={'white'} 
-          multiline
+          multiline = {true}
           onChangeText={text => updateDescription('detail', text)}
           ></TextInput>
       </View>
@@ -56,12 +61,12 @@ export default function Detailjob({route}) {
         style={{fontSize: 17, color: 'white'}} 
         placeholder='กรอกเป็นข้อ1. 2. 3.' 
         placeholderTextColor={'white'} 
-        multiline
+        multiline = {true}
         onChangeText={text => updateDescription('qualification', text)}
         ></TextInput>
       </View>
       <View style={{ marginTop : 5 , width : 100 , alignSelf : 'center' }}>
-        <Button title="ยืนยัน" onPress={() => alert('ยืนยันแล้ว')} />
+        <Button title="ยืนยัน" onPress={handlePostData} />
       </View>
     </ScrollView>
   );
