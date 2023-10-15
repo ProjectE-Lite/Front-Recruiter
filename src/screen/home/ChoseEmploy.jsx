fd444524205557807150d4052172a539d4b3d121
pattarawan21 <nadekopapama@gmail.com>
2023-10-16 00:09:32 +0700

    update delete

--- src/screen/home/ChoseEmploy.jsx
+++ src/screen/home/ChoseEmploy.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Employ from '../money/Employ'
@@ -13,6 +13,7 @@ const ChoseEmploy = ({route}) => {
   const [List_Worker, setList_Worker] = useState([])
   const [key, setKey] = useState("")
   const [state, setState] = useState(0)
+  const [usrStatus, setUsrStatus] = useState([])
   const MAX_NAME_LENGTH = 15
   useEffect(() => {
     axios(`http://${YOURAPI}/works/${work_ID}/status`)
@@ -37,6 +38,7 @@ const ChoseEmploy = ({route}) => {
     axios.get(`http://${YOURAPI}/works/${work_ID}`)
     .then(res => {
       const listusrId = res.data.list_of_worker
+      setUsrStatus(res.data.user_status)
       Promise.all(listusrId.map(usrID => 
         axios.get(`http://${YOURAPI}/users/${usrID}`)
         )
@@ -49,56 +51,70 @@ const ChoseEmploy = ({route}) => {
     )
   }, [state])
 
+  const delete_job = () =>{
+    // axios.delete(`http://${YOURAPI}/works/${work_ID}`)
+    // .then( response =>{
+    navigation.navigate('pageHome')
+  //   console.log('DELETE request successful');
+  // })
+  // .catch(error => {
+  //   console.error('Error making DELETE request:', error);
+  // });
+  
+  };
   const handleImage2Press = () => {
     alert('ไม่รับ');
   };
 
   const handleImage3Press = (usr) => {
-    console.log(usr)
     axios.patch(`http://${YOURAPI}/users/${usr}/accept/${work_ID}`)
-            .then(response => {
-              alert('รับ');
-                console.log('PATCH request สำเร็จ', response.data);
-              })
-              .catch(error => {
-                console.error('เกิดข้อผิดพลาดในการทำ PATCH request', error);
-              });
-  };
-
+    .then(response => {
+      alert('รับ');
+        console.log('PATCH request สำเร็จ', response.data);
+      })
+    .catch(error => {
+      console.error('เกิดข้อผิดพลาดในการทำ PATCH request', error);
+    });
+  };  
   const renderItem = ({ item, index }) => (
-    <TouchableOpacity onPress={() => {navigation.navigate('รายละเอียดพนักงาน', {item, showBut: "1"})}}>
-        <View style={styles.box}>
-        <Image
-            source={{uri : item.image}}
-            style={styles.boxImage}
-        />
-        <View style={{flexDirection: 'column', flexGrow: 2, marginLeft: 5}}>
-        <Text style={{ marginBottom: 5, fontSize: 15, flexShrink: 1}}>
-          {`${item.first_name} ${item.last_name}`.length > MAX_NAME_LENGTH ?
-            `${item.first_name} ${item.last_name}`.substring(0, MAX_NAME_LENGTH) + '...' :
-            `${item.first_name} ${item.last_name}`}
-        </Text>
-        </View>
-        <TouchableOpacity onPress={() =>{}} >
-            <Image
-            source={require('../../assets/image/Ayellow.png')}
-            style={styles.imageButton}
-            />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleImage2Press}>
-            <Image
-            source={require('../../assets/image/Redx.png')}
-            style={styles.imageButton}
-            />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleImage3Press (item._id)}>
-            <Image
-            source={require('../../assets/image/Correct.png')}
-            style={styles.imageButton}
-            />
-        </TouchableOpacity>
-        </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('รายละเอียดพนักงาน', {item, showBut: "1", work_ID: work_ID})
+        }}>
+          <View style={styles.box}>
+          <Image
+              source={{uri : item.image}}
+              style={styles.boxImage}
+          />
+          <View style={{flexDirection: 'column', flexGrow: 2, marginLeft: 5}}>
+          <Text style={{ marginBottom: 5, fontSize: 15, flexShrink: 1}}>
+            {`${item.first_name} ${item.last_name}`.length > MAX_NAME_LENGTH ?
+              `${item.first_name} ${item.last_name}`.substring(0, MAX_NAME_LENGTH) + '...' :
+              `${item.first_name} ${item.last_name}`}
+          </Text>
+          {usrStatus[item._id] && usrStatus[item._id].interview_appointment !== null ?(
+            <Text>{`Interview Appointment:${'\n'}วันที่ :${usrStatus[item._id].interview_appointment.slice(0,10)}${'\n'}เวลา : ${usrStatus[item._id].interview_appointment.slice(10,21)}`}</Text>
+          ) : (
+            <>
+            <Text>ยังไม่มีเวลานัดหมาย</Text>
+            </>
+          )}
+          </View>
+          <TouchableOpacity onPress={handleImage2Press}>
+              <Image
+              source={require('../../assets/image/Redx.png')}
+              style={styles.imageButton}
+              />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleImage3Press (item._id)}>
+              <Image
+              source={require('../../assets/image/Correct.png')}
+              style={styles.imageButton}
+              />
+          </TouchableOpacity>
+          </View>
+      </TouchableOpacity>
+    </>
   );
 
   const RenderUsrWork = ({item, index}) => (
@@ -121,6 +137,13 @@ const ChoseEmploy = ({route}) => {
 
   return (
     <>
+       <View style={{marginLeft:350}}>
+        <TouchableOpacity   onPress={delete_job}>
+          <View >
+          <Text style={{fontSize:18,color:'red'}}>ลบงาน</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     {key == "still_choosing" ? (
       <View style={styles.container}>
         <Text style={{ fontSize: 18 }}>{name} - <Text style={{ color: 'red' }}>{type_of_work}</Text></Text>
@@ -130,6 +153,7 @@ const ChoseEmploy = ({route}) => {
               <Text>ผู้สมัคร</Text>
             </View>
           </TouchableOpacity>
+
           <TouchableOpacity onPress={() => {setState(1)}}>
             <View style={{backgroundColor: 'green', height: 40, width: 175, borderRadius: 10, marginTop: 5, alignItems: 'center', justifyContent: 'center'}}>
               <Text>รับเข้าทำงาน</Text>
@@ -151,8 +175,10 @@ const ChoseEmploy = ({route}) => {
                 <Text>ไม่มีผู้สมัคร</Text>
               </View>
             )}
+            
             keyExtractor={(item) => item._id}
             />
+            
          </>
         ) : (
           <>
@@ -173,11 +199,13 @@ const ChoseEmploy = ({route}) => {
             />
           </>
         )}
+        
     </View>
       ) 
     : (
       <Employ navigation={navigation} userData={userData} work_ID= {work_ID}></Employ>
       )}
+      
     </>
   )
 }
@@ -214,7 +242,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 2,
     borderRadius: 10
   },
-});
+  });
 
 
 export default ChoseEmploy
